8b49a7602e1978d4e2c57af81c907e20f4eacdaa
Florencia Oviedo <flor.oviedo2010@gmail.com>
2026-02-03 23:55:19 -0300

    ejercicio letras por numeros

--- letras_por_numeros.py
+++ letras_por_numeros.py
@@ -3,7 +3,6 @@ Docstring for letras_por_numeros
 Reemplazar las letras de un string por su index en el alfabeto (e.g. A = 1 , B = 2 , C = 3 ...)
 
 Consideraciones Adicionales:
-
 Ignorar espacios.
 Hacer clean up del string antes de comenzar el swap (para eliminar acentos y caracteres especiales, se sugiere meter en este proceso de clean up 
 el ignorado de espacios).
